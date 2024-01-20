5b5d0f21b14418fddfd23bce7c6f1eead82a8f29
RitchielRez <ritchielrez@gmail.com>
2024-01-21 00:18:41 +0600

    chore: game is won when all bricks have been destroyed

--- ts/app.ts
+++ ts/app.ts
@@ -87,6 +87,10 @@ function brickCollisionDetection() {
                 ballSpeedY = -ballSpeedY
                 brick.status = 0;
                 score++;
+
+                if (score === brickRowCount * brickColumnCount) {
+                    gameWon();
+                }
             }
         }
     }
